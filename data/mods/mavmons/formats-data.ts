05558bceb4c0994edb52b4901cbca5660ab1528e
LillieCharms <98361161+LillieCharms@users.noreply.github.com>
2024-08-17 04:20:49 -0400

    mavmons test 1

---
+++ data/mods/mavmons/formats-data.ts
@@ -0,0 +1,7 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	mrbeast: {
+		tier: "MV OU",
+		doublesTier: "DMV OU",
+	},
+	
+};
---
+++ data/mods/mavmons/pokedex.ts
@@ -0,0 +1,11 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	mrbeast: {
+		num: 1,
+		name: "<Mr Beast>",
+		types: ["Normal"],
+		gender: "M",
+		baseStats: {hp: 70, atk: 105, def: 78, spa: 78, spd: 94, spe: 107},
+		abilities: {0: "Overgrow", H: "Regenerator"},
+		weightkg: 85,
+	},
+};
