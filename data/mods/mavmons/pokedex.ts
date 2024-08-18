f51adda1826200bfda430d33ef451f2c138d95ad
LillieCharms <98361161+LillieCharms@users.noreply.github.com>
2024-08-17 20:43:28 -0400

    testing 3 types

--- data/mods/mavmons/pokedex.ts
+++ data/mods/mavmons/pokedex.ts
@@ -2,7 +2,7 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	mrbeast: {
 		num: 1,
 		name: "<Mr Beast>",
-		types: ["Normal"],
+		types: ["Normal", "Fairy", "Fighting"],
 		gender: "M",
 		baseStats: {hp: 70, atk: 105, def: 78, spa: 78, spd: 94, spe: 107},
 		abilities: {0: "Overgrow", H: "Regenerator"},
